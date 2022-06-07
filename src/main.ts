214b514aae2ad9e3ed9ada1bc566e33d379ff917
sinbox0701 <psh090953@gmail.com>
2022-06-07 11:35:24 +0900

    DTO & Validation 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //validation
+      forbidNonWhitelisted: true, //validation
+      transform: true, //data type transfer
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
